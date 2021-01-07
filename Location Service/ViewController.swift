53d7c843b10373fbef83e12bd9f4af59c6e3f33a
Syahrizal Akbar <syahrizal@udacoding.com>
2021-01-07 11:17:33 +0700

    Initial Commit

---
+++ Location Service/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Location Service
+//
+//  Created by IDOUDA on 07/01/21.
+//  Copyright © 2021 IDOUDA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
